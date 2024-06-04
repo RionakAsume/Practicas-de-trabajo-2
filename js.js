68b0b4d784037594d2d1deeb0e3f3a9d94125ce7
RionakAsume <imlauerpablo@gmail.com>
2024-06-04 04:01:24 -0300

    arregle el codigo y le añadi mas ejemplos de los datos

--- js.js
+++ js.js
@@ -1,33 +1,208 @@
 function datosPrueba() {
 
-    const alumnos = [{
-        NombreProduc: "Collar Asteca",
-        Descripcion: "piedra tallada a mano por un indigena",
-        Precio: 458,
-        Categoria: "Si",
-        Proveedor: "Un Ramdon",
-        CantidadInicialStock: 10,
-
-    },
-    {
-        NombreProduc: "Collar Asteca",
-        Descripcion: "piedra tallada a mano por un indigena",
-        Precio: 458,
-        Categoria: "Si",
-        Proveedor: "Un Ramdon",
-        CantidadInicialStock: 10,
-
-    },
-    {
-        NombreProduc: "Collar Asteca",
-        Descripcion: "piedra tallada a mano por un indigena",
-        Precio: 458,
-        Categoria: "Si",
-        Proveedor: "Un Ramdon",
-        CantidadInicialStock: 10,
-
-    }]
-    return alumnos
+    const productos = [
+        {
+            nombre: "Producto 1",
+            descripcion: "Descripción del producto 1",
+            precio: 10.99,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor A",
+            cantidadStock: 100
+        },
+        {
+            nombre: "Producto 2",
+            descripcion: "Descripción del producto 2",
+            precio: 15.49,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor B",
+            cantidadStock: 200
+        },
+        {
+            nombre: "Producto 3",
+            descripcion: "Descripción del producto 3",
+            precio: 9.99,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor A",
+            cantidadStock: 150
+        },
+        {
+            nombre: "Producto 4",
+            descripcion: "Descripción del producto 4",
+            precio: 20.00,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 75
+        },
+        {
+            nombre: "Producto 5",
+            descripcion: "Descripción del producto 5",
+            precio: 8.50,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor D",
+            cantidadStock: 300
+        },
+        {
+            nombre: "Producto 6",
+            descripcion: "Descripción del producto 6",
+            precio: 22.30,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor B",
+            cantidadStock: 120
+        },
+        {
+            nombre: "Producto 7",
+            descripcion: "Descripción del producto 7",
+            precio: 14.75,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 80
+        },
+        {
+            nombre: "Producto 8",
+            descripcion: "Descripción del producto 8",
+            precio: 11.00,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor A",
+            cantidadStock: 60
+        },
+        {
+            nombre: "Producto 9",
+            descripcion: "Descripción del producto 9",
+            precio: 19.99,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor D",
+            cantidadStock: 140
+        },
+        {
+            nombre: "Producto 10",
+            descripcion: "Descripción del producto 10",
+            precio: 13.49,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 90
+        },
+        {
+            nombre: "Producto 11",
+            descripcion: "Descripción del producto 11",
+            precio: 17.99,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor B",
+            cantidadStock: 110
+        },
+        {
+            nombre: "Producto 12",
+            descripcion: "Descripción del producto 12",
+            precio: 5.99,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor A",
+            cantidadStock: 250
+        },
+        {
+            nombre: "Producto 13",
+            descripcion: "Descripción del producto 13",
+            precio: 23.49,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 70
+        },
+        {
+            nombre: "Producto 14",
+            descripcion: "Descripción del producto 14",
+            precio: 16.75,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor D",
+            cantidadStock: 95
+        },
+        {
+            nombre: "Producto 15",
+            descripcion: "Descripción del producto 15",
+            precio: 12.30,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor B",
+            cantidadStock: 130
+        },
+        {
+            nombre: "Producto 16",
+            descripcion: "Descripción del producto 16",
+            precio: 18.50,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 85
+        },
+        {
+            nombre: "Producto 17",
+            descripcion: "Descripción del producto 17",
+            precio: 21.99,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor A",
+            cantidadStock: 110
+        },
+        {
+            nombre: "Producto 18",
+            descripcion: "Descripción del producto 18",
+            precio: 9.49,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor D",
+            cantidadStock: 190
+        },
+        {
+            nombre: "Producto 19",
+            descripcion: "Descripción del producto 19",
+            precio: 24.99,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 60
+        },
+        {
+            nombre: "Producto 20",
+            descripcion: "Descripción del producto 20",
+            precio: 13.99,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor B",
+            cantidadStock: 105
+        },
+        {
+            nombre: "Producto 21",
+            descripcion: "Descripción del producto 21",
+            precio: 6.75,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor A",
+            cantidadStock: 240
+        },
+        {
+            nombre: "Producto 22",
+            descripcion: "Descripción del producto 22",
+            precio: 22.49,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 75
+        },
+        {
+            nombre: "Producto 23",
+            descripcion: "Descripción del producto 23",
+            precio: 15.30,
+            categoria: "Categoría 2",
+            proveedor: "Proveedor D",
+            cantidadStock: 115
+        },
+        {
+            nombre: "Producto 24",
+            descripcion: "Descripción del producto 24",
+            precio: 10.50,
+            categoria: "Categoría 1",
+            proveedor: "Proveedor B",
+            cantidadStock: 170
+        },
+        {
+            nombre: "Producto 25",
+            descripcion: "Descripción del producto 25",
+            precio: 19.75,
+            categoria: "Categoría 3",
+            proveedor: "Proveedor C",
+            cantidadStock: 95
+        }
+    ];
+    return productos;
 
 }
 
@@ -44,12 +219,12 @@ function tablas() {
     padre.innerHTML = `
     <table>
         <tr>
-            <th>NombreProduc</th>
+            <th>Nombre</th>
             <th>Descripcion</th>
             <th>Precio</th>
-            <th>Categoria/th>
+            <th>Categoria</th>
             <th>Proveedor</th>
-            <th>CantidadInicialStock</th>
+            <th>cantidadStock</th>
            
             
         </tr>
@@ -63,12 +238,12 @@ function tablas() {
     asdasd.forEach(element => {
         const trs = document.createElement('tr');
         trs.innerHTML = `
-        <td>${element.NombreProduc}</td>
-        <td>${element.Descripcion}</td>
-        <td>${element.Precio}</td>
-        <td>${element.Categoria}</td>
-        <td>${element.Proveedor}</td>
-        <td>${element.CantidadInicialStock}</td>
+        <td>${element.nombre}</td>
+        <td>${element.descripcion}</td>
+        <td>${element.precio}</td>
+        <td>${element.categoria}</td>
+        <td>${element.proveedor}</td>
+        <td>${element.cantidadStock}</td>
      
    
         `;
